76468cc48e69296c833aa7dc61d6275e6da137d0
karthick-raj <karthickraj2016@yahoo.com>
2022-03-27 13:53:22 +0200

    Update for cars

--- src/components/Cars/Cars.tsx
+++ src/components/Cars/Cars.tsx
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from "react";
 import {
   Block,
-  Card,
-  CardContent,
-  Col,
   Flex,
-  Grid,
   Icon,
-  Link,
-  Row,
-  Spacer,
-  Text,
-  View,
 } from "vcc-ui";
 import { useSpringCarousel } from "react-spring-carousel";
 import carsList from "../../data/carsList";
@@ -19,18 +10,24 @@ import CarouselItem from "./CarouselItem";
 import CarouselThumb from "./CarouselThumb";
 
 export const Cars: React.FC = () => {
+  const [cars, setCars] = useState<any[]>([]);
   const [activeSlideId, setActiveSlideId] = useState<string>(carsList[0].id);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   (async function () {
-  //     try {
-  //       const cars = await (await fetch("./api/cars.json")).json();
-  //       setCars(cars);
-  //     } catch (e) {
-  //       console.error(e);
-  //     }
-  //   })();
-  // }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    (async function () {
+      try {
+        const cars = await (await fetch("./api/cars.json")).json();
+        setCars(cars);
+        setIsLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+
+
   const {
     carouselFragment,
     slideToPrevItem,
@@ -44,7 +41,7 @@ export const Cars: React.FC = () => {
     shouldResizeOnWindowResize: true,
     withLoop: true,
     withThumbs: true,
-    items: carsList.map((car: any) => ({
+    items: cars.map((car: any) => ({
       id: car.id,
       renderItem: <CarouselItem car={car} />,
       renderThumb: (
@@ -63,22 +60,30 @@ export const Cars: React.FC = () => {
     }
   });
 
+  const renderCars = () =>{
+    return (
+      <Block>
+      <Block>{carouselFragment}</Block>
+       <Flex
+         extend={{ flexDirection: "row", right: "25px", position: "absolute" }}
+       >
+         <div className="carousel-buttons">
+           <button className="prev-next-button" onClick={slideToPrevItem}>
+             <Icon type="media-previous-32"></Icon>
+           </button>
+           <button className="prev-next-button" onClick={slideToNextItem}>
+             <Icon type="media-next-32"></Icon>
+           </button>
+         </div>
+         <div className="thumbs-container">{thumbsFragment}</div>
+       </Flex>
+     </Block>
+    )
+  }
+
   return (
     <Block>
-      {carsList.length ? <Block>{carouselFragment}</Block> : <h1>Loading</h1>}
-      <Flex
-        extend={{ flexDirection: "row", right: "25px", position: "absolute" }}
-      >
-        <div className="carousel-buttons">
-          <button className="prev-next-button" onClick={slideToPrevItem}>
-            <Icon type="media-previous-32"></Icon>
-          </button>
-          <button className="prev-next-button" onClick={slideToNextItem}>
-            <Icon type="media-next-32"></Icon>
-          </button>
-        </div>
-        <div className="thumbs-container">{thumbsFragment}</div>
-      </Flex>
+       {isLoading ? <h1> Is loading!!!!</h1> : renderCars()}
     </Block>
   );
 };
